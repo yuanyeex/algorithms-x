5c52d16695480a872960cac8902d736d0cc43022
yuanyeex <yuanyeex@gmail.com>
2023-02-18 23:46:54 +0800

    [go][prb0633] Sum of Square Numbers

--- leetcode-in-go/solutions/0633.sum-of-square-numbers/sum-of-square-numbers.go
+++ leetcode-in-go/solutions/0633.sum-of-square-numbers/sum-of-square-numbers.go
@@ -2,7 +2,23 @@ package prb0633
 
 import "math"
 
+/**
+ * worst o(sqrt(n))
+ */
 func judgeSquareSum(c int) bool {
+	for m := 0; m*m <= c; m++ {
+		b := math.Sqrt(float64(c - m*m))
+		if b == math.Floor(b) {
+			return true
+		}
+	}
+	return false
+}
+
+/**
+ * O(sqrt(n)): always
+ */
+func judgeSquareSum_2(c int) bool {
 	m, n := 0, int(math.Sqrt(float64(c)))
 	for m <= n {
 		tmp := m*m + n*n
@@ -17,6 +33,9 @@ func judgeSquareSum(c int) bool {
 	return false
 }
 
+/**
+ * O(n)
+ */
 func judgeSquareSum_time_exceeded(c int) bool {
 	m, n := 0, c
 	for m <= n {
--- leetcode-in-go/solutions/0633.sum-of-square-numbers/sum-of-square-numbers_test.go
+++ leetcode-in-go/solutions/0633.sum-of-square-numbers/sum-of-square-numbers_test.go
@@ -16,7 +16,23 @@ func Test_Ok(t *testing.T) {
 	}
 
 	for _, prb := range problems {
-		ret := judgeSquareSum_time_exceeded(prb.input)
+		ret := judgeSquareSum_2(prb.input)
+		if ret != prb.output {
+			t.Errorf("prb %v expect %v, but got %v", prb.input, prb.output, ret)
+		}
+	}
+}
+
+func Test_Ok2(t *testing.T) {
+	problems := []problem{
+		{5, true},
+		{3, false},
+		{1, true},
+		{999999999, false},
+	}
+
+	for _, prb := range problems {
+		ret := judgeSquareSum(prb.input)
 		if ret != prb.output {
 			t.Errorf("prb %v expect %v, but got %v", prb.input, prb.output, ret)
 		}
